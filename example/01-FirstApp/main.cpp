7f2c9b43e41901a54db190880d44b017362c97f6
ben <bbclr20@gmail.com>
2020-10-31 11:02:56 +0800

    update example 01

--- example/01-FirstApp/main.cpp
+++ example/01-FirstApp/main.cpp
@@ -3,10 +3,16 @@
 void displayMe(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-        glVertex3f(0.5, 0.0, 0.5);
         glVertex3f(0.5, 0.0, 0.0);
         glVertex3f(0.0, 0.5, 0.0);
-        glVertex3f(0.0, 0.0, 0.5);
+        glVertex3f(0.0, 0.0, 0.0);
+
+    glEnd();
+   
+    glBegin(GL_POLYGON);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(-0.5, 0.0, 0.0);
+        glVertex3f(0.0, -0.5, 0.0); 
     glEnd();
     glFlush();
 }
